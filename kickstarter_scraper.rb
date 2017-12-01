c5ebfbc22ac8ec39b69b39f4973ef47e8d436446
Chad Montoya <chadmontoya21@yahoo.com>
2017-12-01 20:59:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,16 +10,20 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => pry(main)> project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text
+    }
   end
   projects
   binding.pry
 end
 
 
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: pry(main)> project.css("p.bbcard_blurb").text
-# location: project.css(".location-name").text
+
+
+
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 create_project_hash
